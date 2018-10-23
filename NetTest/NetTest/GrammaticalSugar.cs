bf5abbef5aca01bc03610b1a84fea77f8cff512f
xuxiang <tianyaxx@163.com>
2018-10-23 17:38:47 +0800

    1023

--- NetTest/NetTest/GrammaticalSugar.cs
+++ NetTest/NetTest/GrammaticalSugar.cs
@@ -8,10 +8,53 @@ namespace NetTest
 {
     class GrammaticalSugar
     {
+        //自动属性prop+tab
         public string xuxStr { get;  }
 
+
+        //自动属性prop+tab
         public int MyProperty { get; set; }
 
+        //测试匿名类型
+        private class XuxTest1
+        {
+            public int intTest { get; set; }
+            public string strTest { get; set; }
+            public string testOut { get; set; }
+
+            public XuxTest1()
+            {
+
+            }
+
+            public XuxTest1(int x,string y)
+            {
+                intTest = x;
+                strTest = y;
+
+            }
+
+            public string test()
+            {
+                return intTest.ToString() + "  @ " + strTest; 
+            }
+        }
+
+        private class XuxTest2 : XuxTest1
+        {
+            public double age { get; set; }
+            public XuxTest2(string x, int y = 55)
+            {
+                strTest = x;
+                intTest = y;
+            }
+
+            //public XuxTest1 getTest1()
+            //{
+            //    testOut = "Test2Out";
+            //    return base.
+            //}
+        }
 
         public GrammaticalSugar()
         {
@@ -19,8 +62,16 @@ namespace NetTest
             double xd;
             if (double.TryParse(xuxStr, out xd))
             {
-
+                
             }
+
+            //测试匿名类型,隐式类型
+            var vTest = new XuxTest1(22, "33");
+
+            vTest.test();
+
+
+            
         }
     }
 }

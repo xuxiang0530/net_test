461c7f955df9c4ee8c3ac14eaba7cafb1b98bfa8
xux <tianyaxx@163.com>
2018-10-30 23:33:50 +0800

    1030

--- NetTest/NetTest/ThreadForm.Designer.cs
+++ NetTest/NetTest/ThreadForm.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -49,14 +52,48 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "同步方法";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(274, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "异步方法";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(118, 274);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(211, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "异步有序方法";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // ThreadForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(907, 454);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "ThreadForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ThreadForm";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -67,5 +104,8 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- NetTest/NetTest/ThreadForm.cs
+++ NetTest/NetTest/ThreadForm.cs
@@ -6,12 +6,17 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace NetTest
 {
     public partial class ThreadForm : Form
     {
+        private delegate void DoSomeCallBack(int value,string str);
+
+        DoSomeCallBack doSomeCallBack;
+
         public ThreadForm()
         {
             InitializeComponent();
@@ -19,7 +24,88 @@ namespace NetTest
 
         private void button1_Click(object sender, EventArgs e)
         {
+            doSomeCallBack = new DoSomeCallBack(SetValue);
+            string str = "this is test";
 
+            Thread nT = new Thread(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    textBox1.Invoke(doSomeCallBack, i, str);
+                    Thread.Sleep(1000);
+                }
+            });
+            nT.IsBackground = true;
+            nT.Start();
+        }
+
+        private void SetValue(int value, string str)
+        {
+            this.textBox1.Text = string.Format( "{0} and {1}",value.ToString(),str);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine($"****************btnSync_Click Start {Thread.CurrentThread.ManagedThreadId.ToString("00")} {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}***************");
+            for(int i =0;i<5;i++)
+            {
+                string name = $"btnSync_Click_{i}";
+                this.DoSomethingLong(name);
+            }
+        }
+        
+        //public delegate AsyncCallback(IAsyncResult ar);
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine($"***************btnAsync_Click Start {Thread.CurrentThread.ManagedThreadId}");
+            Action<string> action = this.DoSomethingLong;
+            // 调用委托(同步调用)
+            //action.Invoke("btnAsync_Click_1");
+            // 异步调用委托
+            IAsyncResult ia = null;
+            AsyncCallback callBack = p =>
+            {
+                //Console.WriteLine(object.ReferenceEquals(p,ia));
+                Console.WriteLine($"到这里计算已经完成了。{Thread.CurrentThread.ManagedThreadId.ToString("00")}。");
+            };
+
+
+            //ia = action.BeginInvoke("this is xux test", callBack, null);
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    string name = $"btnSync_Click_{i}";
+            //    ia = action.BeginInvoke(name, callBack, null);
+            //}
+            Console.WriteLine($"***************btnAsync_Click End    {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        private void DoSomethingLong(string name)
+        {
+            Console.WriteLine($"***DoSomethingLong{name} Start {Thread.CurrentThread.ManagedThreadId.ToString("00")}{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}*********");
+
+            long lr = 0;
+            for(int i = 0;i < 1000000000;i++)
+            {
+                lr += i;
+            }
+            Console.WriteLine($"***DoSomethingLong{name} End {Thread.CurrentThread.ManagedThreadId.ToString("00")}{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}{lr}************&&");
+
+        }
+
+
+        private int xuxtest(string str)
+        {
+            return str.Length;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //string str = "aseds";
+            Func<string,int> xuxFunc =  p => p.Length;
+
+            IAsyncResult ias = xuxFunc.BeginInvoke("asas", p => Console.WriteLine(p.AsyncState), $"这是后面");
+
+            Console.WriteLine($"func.EndInvoke(asyncResult)={xuxFunc.EndInvoke(ias)}");
         }
     }
 }
